a744cea4576d781edf658bbf1e321138dd000706
John <john.torres.hurtado@gmail.com>
2017-07-26 10:10:35 -0700

    new changes based on PEP 8

--- media.py
+++ media.py
@@ -1,14 +1,15 @@
 import webbrowser
-#importing webbrowser module
+# Importing webbrowser module
+
 
 class Movie():
-    #this initiates the class behavior adding the information added on entretainment_center.py
+    # This initiates the class behavior. Maps movie data to class.
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #this triggers a webbrowser behavior to open the trailer url from a defined movie title.
+    # This triggers a webbrowser behavior to open the trailer ur.
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
